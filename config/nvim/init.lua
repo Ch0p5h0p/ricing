74d5d627374917bd2e3cab39d13d5add027d23bc
Sam P. <Samjpoyner@icloud.com>
2026-01-23 00:03:18 -0500

    update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,10 @@
 require("config.lazy")
 require("lazy").setup("plugins")
-vim.api.nvim_create_user_command('H', 'Dashboard', {})
-
+vim.api.nvim_create_user_command('CRun', function()
+        local cmd = vim.fn.input("Command: ")
+        if cmd ~= nil and cmd ~= "" then
+                vim.cmd("split | terminal "..cmd)
+        end
+end, {})
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
 vim.cmd([[cd ~]])
