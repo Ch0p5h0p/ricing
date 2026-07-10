e9797c27184ed5a06a145f984446ba3a74ce5f6a
Ch0p5h0p <samjpoyner@icloud.com>
2026-07-09 22:41:39 -0400

    Revert "Add files via upload"
    This reverts commit d3c0d4f7f18e4d0be07fba9ec12368f7dd7ca8f6.

--- hypr/scripts/battery.sh
+++
@@ -1,4 +0,0 @@
-percent=$(cat /sys/class/power_supply/BAT0/capacity)
-status=$(cat /sys/class/power_supply/BAT0/status)
-
-echo "$percent% ($status)"
--- hypr/scripts/cava_to_file.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# ~/.config/hypr/scripts/cava_to_file.sh
-
-RAMFILE="/dev/shm/cava_output.txt"
-
-cava -p /home/Radar77/.config/cava/config_hyprlock | while read -r line; do
-    echo "$line" > "$RAMFILE"
-done
